0fe23ca4971d17bcc99e6ba29da4eb7a7b67c0f7
arrayListTwo <programkxxs@gmail.com>
2024-12-08 08:44:59 +0800

    init

---
+++ app.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <NuxtLayout>
+      <header>
+        <ul>
+          <li>
+            <NuxtLink to="/">Home</NuxtLink>
+            <NuxtLink to="/events" >Events</NuxtLink>
+          </li>
+        </ul>
+      </header>
+      <NuxtPage></NuxtPage>
+    </NuxtLayout>
+  </div>
+</template>
+<script setup lang="ts">
+</script>
---
+++ pages/events/[id].vue
@@ -0,0 +1,12 @@
+<template>
+  <div>event id: {{$route.params.id}}</div>
+</template>
+
+<script setup>
+const route = useRoute()
+
+console.log(route.params)
+
+const nuxt = useNuxtApp()
+console.log(nuxt)
+</script>
